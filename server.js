900309d6f8db5edafc077701d05a19ad3419b93e
hjlee <heunjong.lee@gmail.com>
2024-01-11 02:54:21 -0500

    express middleware  incorporated into server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const db = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
