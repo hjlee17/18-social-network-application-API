ef0f6fd19dadbf829074f89f46630275d90b4cbf
hjlee <heunjong.lee@gmail.com>
2024-01-14 20:05:34 -0500

    set up user and thought routes to use controller functions

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require('../../controllers/thoughtController');
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userController');
+
+module.exports = router;
